284491d1d5fdd3513ec961d3210f500184649d1a
kotako <sy.empress0321@gmail.com>
2018-05-27 01:26:46 +0900

    Modify fish alias 4 git

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,8 +1,16 @@
 # typo
 alias brwe='brew'
 
+# shortcuts
+alias e='exit'
+alias h='cd ~'
+
 # git shortcuts
 alias g='git'
 alias gs='g status'
 alias ga='g add .'
 alias gc='g commit -m'
+alias gr='g reset'
+alias gp='g push'
+alias gl='g pull'
+alias gf='g fetch'
