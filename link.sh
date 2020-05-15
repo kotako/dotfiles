928d4285012fc0e3c8ab4459e32b5766666c8268
kotako <sy.empress0321@gmail.com>
2020-05-15 10:36:17 +0900

    Replace .gitignore_global to config/git

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ln -sf `pwd`/.gitconfig ~/.gitconfig
-ln -sf `pwd`/.gitignore_global ~/.gitignore_global
+ln -snfv `pwd`/git/* ~/.config/git
 ln -sf `pwd`/.zshrc ~/.zshrc
 # ln -snfv `pwd`/fish/* ~/.config/fish
 # ln -snfv `pwd`/atom/* ~/.atom
