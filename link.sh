10644a67ee5f66661dfe509fc6449e19dad22f80
kotako <sy.empress0321@gmail.com>
2021-04-13 14:41:12 +0900

    Use -snfv option

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ln -sf `pwd`/.gitconfig ~/.gitconfig
+ln -snfv `pwd`/.gitconfig ~/.gitconfig
 ln -snfv `pwd`/git/* ~/.config/git
-ln -sf `pwd`/.zshrc ~/.zshrc
+ln -snfv `pwd`/.zshrc ~/.zshrc
 # ln -snfv `pwd`/fish/* ~/.config/fish
 # ln -snfv `pwd`/atom/* ~/.atom
