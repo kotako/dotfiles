ea0fd4f1ee6a862982693a6573432ad78e5f93d8
kotako <sy.empress0321@gmail.com>
2021-04-13 15:00:40 +0900

    Use for loop to create symlink

--- link.sh
+++ link.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-ln -snfv `pwd`/.gitconfig ~/.gitconfig
+IGNORE_PATTERN = "^\.(git)"
+
+echo "Create dotfile link"
+for dotfile in .??*
+do
+    [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
+    ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
+done
+
 ln -snfv `pwd`/git/* ~/.config/git
-ln -snfv `pwd`/.zshrc ~/.zshrc
 # ln -snfv `pwd`/fish/* ~/.config/fish
 # ln -snfv `pwd`/atom/* ~/.atom
