a01153b45dfabe9d0d9600424c40e29589d15bcb
kotako <sy.empress0321@gmail.com>
2021-04-18 21:26:23 +0900

    Modify link.sh

--- link.sh
+++ link.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-IGNORE_PATTERN = "^\.(git)"
+IGNORE_PATTERN="^\.(gitignore)"
 
-echo "Create dotfile link"
+echo "Create dotfile link..."
 for dotfile in .??*
 do
     [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
     ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
 done
-
 ln -snfv `pwd`/git/* ~/.config/git
-
-# shell とか使うのを選択式にしてもいいかも？
-# ln -snfv `pwd`/fish/* ~/.config/fish
-# ln -snfv `pwd`/atom/* ~/.atom
+echo "Done"
