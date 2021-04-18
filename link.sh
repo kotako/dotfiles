e9a04adc16918a0d9df80114bde2d5066a523514
kotako <sy.empress0321@gmail.com>
2021-04-18 21:42:51 +0900

    Install zsh

--- link.sh
+++ link.sh
@@ -13,9 +13,17 @@ echo "Done"
 
 echo "Install Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 echo "Update Homebrew..."
 brew update && brew upgrade && brew cask upgrade
-
 echo "Install formulaes, casks..."
 brew bundle --global
+echo "Done"
+
+echo "Change shell to zsh"
+if [ ${SHELL} != "/usr/local/bin/zsh"  ]; then
+    if [ -z `grep "/usr/local/bin/zsh" /etc/shells` ]; then
+        echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
+    fi
+    chsh -s /usr/local/bin/zsh
+fi
+echo "Done"
